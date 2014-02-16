0a99aa7b63111b4616d9f83161ad24de8b313025
Andreas Søvik <arsovik@gmail.com>
2014-02-16 17:04:17 +0100

    Improvements

--- battery.lua
+++ battery.lua
@@ -1,25 +1,49 @@
 local awful = require("awful")
 local io = require("io")
+local naughty = require("naughty")
+local beautiful = require("beautiful")
 local battery = {}
+local warned = false;
 
 local function execute_command(command)
     local fh = io.popen(command)
     local str = fh:read("*all")
     io.close(fh)
-    return str
+    return tostring(str)
+end
+
+local function get_battery_level()
+    local status = execute_command("bash -c \"acpi | sed -nr 's/.* ([0-9]+)%.*/\\1/p'\"")
+
+    if tonumber(status) < 20 and warned == false then
+        naughty.notify({ title    = "Battery Warning"
+        , text     = "Battery is low! - " .. status .. "%"
+        , timeout  = 5
+        , position = "top_right"
+        , fg       = beautiful.fg_focus
+        , bg       = beautiful.bg_focus})
+
+        warned = true;
+    end
+
+    return tostring(status)
 end
 
 battery.add = function(wibox)
     mybatterypanel = wibox.widget.textbox()
     mybatterypanel:set_text("")
 
-    batterytimer = timer({ timeout = 10 })
-    batterytimer:connect_signal("timeout", function() mybatterypanel:set_text(execute_command("bash -c \"acpi | sed -nr 's/.* ([0-9]+%).*/\\1/p'\"")) end)
+    batterytimer = timer({ timeout = 1 })
+
+    batterytimer:connect_signal("timeout", function()
+        local statusBattery = get_battery_level() .. '%'
+        mybatterypanel:set_text(statusBattery)
+    end)
 
     mybatterypanel_t = awful.tooltip({
         objects = { mybatterypanel }, timer_function = function() return execute_command("acpi") end})
 
-    batterytimer:start()
+   batterytimer:start()
 end
 
 return battery
